3f64846e3bd40e15cbceed84078468fdb9580504
Andres Sosa <ramonsosadiaz@gmail.com>
2019-08-19 00:25:45 -0600

    first commit

---
+++ ejercicio1/data_saldos.js
@@ -0,0 +1,178 @@
+var clientes = [
+  {
+    id: 1,
+    nombre: "Andres",
+    saldo: [
+      {
+        id: "hipoteca",
+        saldo: 3000
+      },
+      {
+        id: "tarjeta MM",
+        saldo: 5000
+      },
+      {
+        id: "tarjeta VISA",
+        saldo: 4000
+      }
+    ]
+  },
+  {
+    id: 2,
+    nombre: "Carlos",
+    saldo: [
+      {
+        id: "hipoteca",
+        saldo: 2350
+      },
+      {
+        id: "tarjeta MM",
+        saldo: 3428
+      },
+      {
+        id: "tarjeta VISA",
+        saldo: 12890
+      }
+    ]
+  },
+  {
+    id: 3,
+    nombre: "Cesar",
+    saldo: [
+      {
+        id: "tarjeta MM",
+        saldo: 2389
+      },
+      {
+        id: "tarjeta VISA",
+        saldo: 9973
+      }
+    ]
+  },
+  {
+    id: 4,
+    nombre: "Raul",
+    saldo: [
+      {
+        id: "hipoteca",
+        saldo: 3828
+      },
+      {
+        id: "tarjeta MM",
+        saldo: 50200
+      },
+      {
+        id: "tarjeta VISA",
+        saldo: 23000
+      }
+    ]
+  },
+  {
+    id: 5,
+    nombre: "Marcos",
+    saldo: [
+      {
+        id: "hipoteca",
+        saldo: 1232
+      },
+      {
+        id: "tarjeta MM",
+        saldo: 3378
+      },
+      {
+        id: "tarjeta VISA",
+        saldo: 23787
+      }
+    ]
+  },
+  {
+    id: 6,
+    nombre: "Lissa",
+    saldo: [
+      {
+        id: "hipoteca",
+        saldo: 12000
+      },
+      {
+        id: "tarjeta MM",
+        saldo: 1290
+      },
+      {
+        id: "tarjeta VISA",
+        saldo: 390
+      }
+    ]
+  },
+  {
+    id: 7,
+    nombre: "Karla",
+    saldo: [
+      {
+        id: "hipoteca",
+        saldo: 120000
+      },
+      {
+        id: "tarjeta MM",
+        saldo: 2389
+      },
+      {
+        id: "tarjeta VISA",
+        saldo: 198
+      }
+    ]
+  },
+  {
+    id: 8,
+    nombre: "Patty",
+    saldo: [
+      {
+        id: "hipoteca",
+        saldo: 237809
+      },
+      {
+        id: "tarjeta MM",
+        saldo: 2387
+      },
+      {
+        id: "tarjeta VISA",
+        saldo: 280
+      }
+    ]
+  },
+  {
+    id: 9,
+    nombre: "Vilma",
+    saldo: [
+      {
+        id: "hipoteca",
+        saldo: 145889
+      },
+      {
+        id: "tarjeta MM",
+        saldo: 2000
+      },
+      {
+        id: "tarjeta VISA",
+        saldo: 1290
+      }
+    ]
+  },
+  {
+    id: 10,
+    nombre: "Carolina",
+    saldo: [
+      {
+        id: "hipoteca",
+        saldo: 350000
+      },
+      {
+        id: "tarjeta MM",
+        saldo: 12659
+      },
+      {
+        id: "tarjeta VISA",
+        saldo: 1578
+      }
+    ]
+  }
+];
---
+++ ejercicio1/data_saldos2.js
@@ -0,0 +1,12 @@
+var cliente2 = [
+  { id: 1, nombre: "Andres", saldo: [3000, 5000, 4000] },
+  { id: 2, nombre: "Carlos", saldo: [2350, 3428, 12890] },
+  { id: 3, nombre: "Cesar", saldo: [2389, 9973] },
+  { id: 4, nombre: "Raul", saldo: [3828, 50200, 23000] },
+  { id: 5, nombre: "Marcos", saldo: [1232, 3378, 23787] },
+  { id: 6, nombre: "Lissa", saldo: [12000, 1290, 390] },
+  { id: 7, nombre: "Karla", saldo: [120000, 2389, 198] },
+  { id: 8, nombre: "Patty", saldo: [237809, 2387, 280] },
+  { id: 9, nombre: "Vilma", saldo: [145889, 2000, 1290] },
+  { id: 10, nombre: "Carolina", saldo: [350000, 12659, 1578] }
+];
