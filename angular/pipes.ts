4650428f69bd44c8d064f9175d9b4597aa9e9294
Andres Sosa <ramonsosadiaz@gmail.com>
2019-09-10 08:39:25 -0600

    pipes

---
+++ angular/pipes.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filesize'
+})
+export class FilesizePipe implements PipeTransform {
+
+  transform(bytes: any, decimals: number = 2, ...args: any[]): any {
+
+    if (bytes === 0) { return '0 Bytes'; }
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  }
+
+}
